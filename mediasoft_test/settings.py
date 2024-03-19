5e0b97eccaef70d385c8774ad5186aed024ab3e4
u-shev <sheveleva.jul@gmail.com>
2024-03-19 15:38:43 +0300

    exeption handler

--- mediasoft_test/settings.py
+++ mediasoft_test/settings.py
@@ -102,7 +102,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    ],
+    'EXCEPTION_HANDLER': 'rest_framework.views.exceptions.'
 }
 
 # Internationalization
